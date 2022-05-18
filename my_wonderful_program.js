73109ddfffafe4d4e72ffb7635c070e3237f7214
Matrice-ecole <kamal.guidadou>
2022-05-18 14:36:01 +0200

    Added clipboard

--- my_wonderful_program.js
+++ my_wonderful_program.js
@@ -1,2 +1,20 @@
+import clipboardy from 'clipboardy'
+
 console.log('Node rocks!');
 
+function makePwd(length) {
+    
+    // let length = process.argv
+    let result  = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+console.log(makePwd(5));
+
+//// penser à yargs = process.argv ?    
